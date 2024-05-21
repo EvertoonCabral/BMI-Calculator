71850674c35cb8bf564b99bf7103d2d94ceab331
Everton Cabral <96452540+EvertoonCabral@users.noreply.github.com>
2024-05-20 23:28:44 -0300

    Ended exercice
    Finalized mini imc calculator project, used Intent, putExtra, bundle and type conversions

--- app/src/main/java/com/everton/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/everton/bmicalculator/MainActivity.kt
@@ -3,8 +3,10 @@ package com.everton.bmicalculator
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.everton.bmicalculator.databinding.ActivityMainBinding
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +18,26 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+
+
         binding.btnCalculate.setOnClickListener {
 
             val intent = Intent(this, ResultScreem::class.java)
+
+
+            val weight  = binding.edWeight.text.toString()
+            val height  =  binding.edHeight.text.toString()
+
+            Log.i("Teste", "Height is $height and Weight is $weight")
+
+
+            if (weight.isNotEmpty() && height.isNotEmpty()){
+
+                intent.putExtra("height", height)
+                intent.putExtra("weight", weight)
+
+
+            }
 
             startActivity(intent)
 
--- app/src/main/java/com/everton/bmicalculator/ResultScreem.kt
+++ app/src/main/java/com/everton/bmicalculator/ResultScreem.kt
@@ -1,7 +1,9 @@
 package com.everton.bmicalculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.everton.bmicalculator.databinding.ActivityMainBinding
 import com.everton.bmicalculator.databinding.ActivityResultScreemBinding
 
@@ -9,6 +11,7 @@ class ResultScreem : AppCompatActivity() {
 
     private lateinit var binding: ActivityResultScreemBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResultScreemBinding.inflate(layoutInflater)
@@ -16,7 +19,38 @@ class ResultScreem : AppCompatActivity() {
         setContentView(view)
 
 
+        val bundle = intent.extras
+        if( bundle != null ){
+
+            var weight = bundle.getString("weight")
+            var height = bundle.getString("height")
+
+            Log.i("Teste ResultScreem", "Height is $height and Weight is $weight")
+
+
+            binding.txtWeight.text = "Peso informado $weight kg"
+            binding.txtHeight.text  = "Altura informada $height m"
+
+
+            val imc = weight!!.toDouble() / (height!!.toDouble() * height.toDouble())
+
+            val resultado = if( imc < 18.5 ){
+                "Lean"
+            }else if( imc in 18.5 .. 24.9 ){
+                "Normal"
+            }else if( imc in 25.0 .. 29.9 ){
+                "Soreweight"
+            }else{
+                "Obese"
+            }
+            binding.txtDiagnosisResult.text = resultado
+
+        }
+
+
+        }
+
+
         
 
     }
-}
